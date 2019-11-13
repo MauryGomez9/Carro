c260956432e2f0e285ae1a5a8118570aacc9045a
Mauricio Gomez <mauriciogotze.9@yahoo.com>
2019-11-13 15:40:19 -0600

    tercero commit
    creacion de lista modelo completo

---
+++ models/carro.model.php
@@ -0,0 +1,56 @@
+<?php
+  require_once "libs/dao.php";
+
+
+  function obtenerListas()
+  {
+      $sqlstr = "select `carro`.`idcarro`,
+          `carro`.`carroNombre`,
+          `carro`.`carroPeso`,
+          `carro`.`carroModelo`
+      from `carro`.`carro`";
+
+      $carro = array();
+      $carro = obtenerRegistros($sqlstr);
+      return $carro;
+  }
+
+  /************************************************/
+  function obtenerModeloPorId($id)
+{
+  $sqlstr="select `carro`.`carroModelo`
+        from `carro`.`carro` where idcarro=%d";
+  $carro= array();
+  $carro=obtenerUnRegistro(sprintf($sqlstr, $id));
+  return $carro;
+}
+
+
+function obtenerModelo()
+{
+    return array(
+        array("cod"=>"CRV", "dsc"=>"Lujo"),
+        array("cod"=>"XL", "dsc"=>"Deportivo"),
+        array("cod"=>"EX", "dsc"=>"Extra")
+    );
+}
+
+function agregarNuevoCarro($nombre, $peso, $modelo) {
+    $insSql = "INSERT INTO carro(carroNombre, carroPeso, carroModelo)
+      values ('%s', %f, '%s');";
+      if (ejecutarNonQuery(
+          sprintf(
+              $insSql,
+              $nombre,
+              $peso,
+              $modelo
+          )))
+      {
+        return getLastInserId();
+      } else {
+          return false;
+      }
+}
+
+
+?>
